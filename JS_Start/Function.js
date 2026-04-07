4ffaa3df759a51f2740529b86f7745c930ad1d8a
Shivam2807code <shivamsah4567@gmail.com>
2026-04-07 17:01:12 +0000

    CommitDone

---
+++ JS_Start/Function.js
@@ -0,0 +1,71 @@
+// function sayName(){
+//     console.log("S")
+//     console.log("h")
+//     console.log("i")
+//     console.log("v")
+//     console.log("a")
+//     console.log("m")
+// }
+
+// sayName();
+
+// function add(num1,num2){
+//     return num1+num2;
+// }
+
+// console.log(add(5,7))
+
+
+// function isLogged(name){
+//     return `${name} is just logged in this site`
+// }
+
+// console.log(isLogged("Shivam"))
+
+
+// function isLogged(name){
+//     if(!name) return;
+//     return `${name} is just logged in this site`
+// }
+
+// console.log(isLogged())
+// console.log(isLogged("Shivam"))
+// console.log(isLogged(""))
+
+// const cart={
+//     Shoe:"Nike",
+//     Phone:"Iphone 17 pro"
+// }
+
+// function ShopingCart(cart){
+// return `The person is buy ${cart.Shoe} shoe and ${cart.Phone} For using this`
+// }
+
+// console.log(ShopingCart(cart))
+
+
+
+
+// function ShopingCart(cart){
+// return `The person is buy ${cart.Shoe} shoe and ${cart.Phone} For his daily uses`
+// }
+
+// console.log(ShopingCart({
+//     Shoe:"Adidas",
+//     Phone:"Samsung S26 ultra"
+// }))
+
+
+// function SecondElement(cart){
+// return cart[1];
+// }
+
+// console.log(SecondElement([
+//     101,201,301,401,501]))
+
+const num=[101,400,600,200];
+function findNum(num){
+ return num[3]
+}
+
+console.log(findNum(num))
