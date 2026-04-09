88c888027f28cc036b19f6c72f967f9dc12b93ee
Shivam2807code <shivamsah4567@gmail.com>
2026-04-09 17:58:34 +0000

    ThisKeyword

---
+++ JS_Start/ThikKeyword.js
@@ -0,0 +1,47 @@
+// const data={
+//     name:"Shivam Kumar",
+//     user:function (){
+//    console.log(this.name);
+//     }
+// }
+
+// data.name="Ramu";
+// data.user();
+
+// function name(){
+//     const name="Shivam Kumar"
+//     // console.log(this.name)
+//     console.log(this)
+// }
+// name();
+
+// const abc=function (){
+//     const name="Shivam"
+//     // console.log(this.name)
+//     // console.log(name)
+//     console.log(this)
+
+// }
+// abc();
+
+// const abc=()=>{
+//     const name="Shivam Kumar"
+//     // console.log(name);
+//     // console.log(this.name);
+//     console.log(this);
+// }
+
+// abc()
+
+
+
+// IIFE (Imeditely Invoked Function)
+//  Named IIFE
+// (function name(){
+//     console.log("Shivam Kumar") 
+// })();
+
+// ((name)=>{
+//     console.log(`${name}`)  
+// })("Lucky Sah") 
+// Withot Named IIFE
