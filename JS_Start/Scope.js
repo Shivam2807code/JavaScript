85faa7c99787e9d0e87bab9ad65a59667ea04f45
Shivam2807code <shivamsah4567@gmail.com>
2026-04-09 17:22:37 +0000

    Scope

---
+++ JS_Start/Scope.js
@@ -0,0 +1,41 @@
+// console.log(a)
+// var a="Cat";  Functional Scoped
+
+// console.log(a)
+// let a="Cat"; Blocked Scoped
+
+// console.log(a)
+// const a="Cat"; Blocked Scoped
+
+// function abc(){
+//     if(true){
+//  a=10;
+//     }
+    
+// }
+// abc();
+// console.log(a);
+// if(true){
+//     var a=20;
+//     console.log(a);
+// }
+
+// console.log(a)
+
+// function test() {
+//     if (true) {
+//         a = 20;
+//     }
+// }
+
+// test();
+// console.log(a);
+
+// function xyz(){
+//     x=20;
+// }
+
+// x=40
+// xyz();
+// x=30
+// console.log(x);
