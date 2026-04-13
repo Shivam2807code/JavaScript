4b5dba552b147ec6d54c3e8b463bd0d3b98bed93
Shivam2807code <shivamsah4567@gmail.com>
2026-04-13 17:40:22 +0000

    Loop

---
+++ JS_Start/Loop.js
@@ -0,0 +1,73 @@
+// For Loop
+// for(let i=1;i<=10;i++){
+//     console.log(`${i}`)
+// }
+
+// for(let i=0;i<=10;i++){
+//     if(i==10){
+//         console.log("Number is completed");
+//     }
+//     console.log(`${i}`);
+// }
+
+// for(let i=1;i<=10;i++){
+//     console.log(`Outer loop start from ${i}`)
+//     for(let j=1;j<=10;j++){
+//         console.log(`${i}*${j} = `,i*j);
+//     }
+// }
+
+
+// for(let i=1;i<=20;i++){
+//     if(i==5){
+//         console.log(`${i} is matched`)
+//     }
+//     console.log(i);
+// }
+
+// for(let i=1;i<=20;i++){
+//     if(i==5){
+//         console.log(`${i} is matched`)
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// for(let i=1;i<=20;i++){
+//     if(i==5){
+//         console.log(`${i} is matched`)
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// While Loop
+
+// let a=0;
+// while(a<=10){
+//      console.log(a);
+//       a+=2;
+// }
+
+// Do While
+// let a=10;
+// do{
+//     console.log(a);
+//     a++;
+// }while(a<=20)
+
+// let arr=["flash","darkSide","supermen"]
+// let a=0;
+// do{
+// console.log(arr[a]);
+// a++;
+// }while(a<arr.length)
+
+// pattern printing
+
+// for(let i=0;i<5;i++){
+//     for(let j=0;j<=i;j++){
+//         console.log("*");
+//     }
+//     console.log();
+// }
