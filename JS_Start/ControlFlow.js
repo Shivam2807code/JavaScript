f8679f616fdb3e3b3f0d92cf9a6102f534cd4384
Shivam2807code <shivamsah4567@gmail.com>
2026-04-13 16:53:08 +0000

    ControlFlow

---
+++ JS_Start/ControlFlow.js
@@ -0,0 +1,90 @@
+// const name="Shivam Kumar";
+// const age=22;
+// if(age){
+//     console.log(`My name is ${name} and I'm ${age} old`)
+// }
+
+// const month=11
+
+// switch (month){
+//     case 1:
+//         console.log("January");
+//         break;
+//          case 2:
+//         console.log("February");
+//         break;
+//          case 3:
+//         console.log(
+//             "March");
+//         break;
+//          case 4:
+//         console.log("April");
+//         break;
+//          case 5:
+//         console.log("May");
+//         break;
+//          case 6:
+//         console.log("June");
+//         break;
+//          case 7:
+//         console.log("July");
+//         break;
+//          case 8:
+//         console.log("August");
+//         break;
+//          case 9:
+//         console.log("September");
+//         break;
+//          case 10:
+//         console.log("October");
+//         break;
+//          case 11:
+//         console.log("November");
+//         break;
+//          case 12:
+//         console.log("December");
+//         break;
+//         default:
+//             console.log("Error")
+// }
+
+/* Falsy Value
+False,0,-0,BigInt 0n,"",null,undefinied,NaN
+*/
+
+/*Truthy Value
+" ", "false","0",[],{},function (){}
+*/ 
+
+
+// const abc=0;
+// const xyz="";
+//  if(abc==xyz){
+//     console.log("True");
+//   } 
+//  else
+//     console.log("False")
+
+
+// Null Coalescing Operator (??): null undefinied
+// let val;
+// val=5??10;
+// console.log(val)
+// let val1;
+// val1=NaN??40;
+// console.log(val1)
+// val1=null??20;
+// console.log(val1)
+// val1=undefined??60;
+// console.log(val1)
+// val1=undefined??70??80;
+// console.log(val1)
+
+
+// Ternary Operator
+// let price=100;
+// price>=100?console.log(`Price is ${price}`):console.log(`Price is less then ${price}`)
+
+/*Operator
+ <,>,<=,>=,==,===,!=,!==
+*/
